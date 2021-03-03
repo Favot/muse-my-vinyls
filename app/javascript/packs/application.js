a28a5766ac306b40a7d55e34177cec6c66cea7b2
christopheherrou <christophe.herrou@gmail.com>
2021-03-03 16:06:57 +0100

    new branch

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,9 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-
+import { initTotalPrice } from '../components/init_total_price';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initTotalPrice()
 });
