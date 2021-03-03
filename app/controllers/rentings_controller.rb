12b3d45ded67ad0fb99f878a29986e4eddc2ad43
lise henry <lise.henry@essca.eu>
2021-03-03 12:45:54 +0100

    finished method Create a renting

--- app/controllers/rentings_controller.rb
+++ app/controllers/rentings_controller.rb
@@ -1,24 +1,23 @@
 class RentingsController < ApplicationController
-
   def create
     @renting = Renting.new(renting_params)
-    @vinyl = vinyl.find(params[:vinyl_id])
+    @vinyl = Vinyl.find(params[:vinyl_id])
     @renting.vinyl = @vinyl
+    @renting.renter = current_user
 
     if @renting.save
       flash[:alert] = 'Request submitted!'
       # when possible redirect to 'All requests'
       redirect_to vinyls_path
     else
-      flash[:alert] = 'Something went wrong.'
-      # when possible redirect to 'All requests'
-      redirect_to vinyls_path
+      flash[:alert] = 'Something went wrong. Try again.'
+      redirect_to vinyl_path(@vinyl)
     end
   end
 
   private
 
   def renting_params
-    params.require(:renting).permit(:start_date, :end_date, :status, :total_price, :renter_id, :vinyl_id)
+    params.require(:renting).permit(:start_date, :end_date, :total_price, :vinyl_id, :renter_id, :status)
   end
 end
