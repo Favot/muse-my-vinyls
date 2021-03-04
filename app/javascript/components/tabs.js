16ea83b07db17cb5cceba4bd79b000d940905485
lise henry <lise.henry@essca.eu>
2021-03-04 12:33:30 +0100

    css for tabs and cards on rentings listing

--- app/javascript/components/tabs.js
+++ app/javascript/components/tabs.js
@@ -1,56 +1,54 @@
 const displayTab = () => {
   // console.log(event.target.id);
 
-if (event.target.id == 'pending-tab') {
+  if (event.target.id == 'pending-tab') {
 
-  // Remove .show.active on all tabs
-  const hiddenATab = document.querySelector('#accepted');
-  hiddenATab.classList.remove("show");
-  hiddenATab.classList.remove("active");
+    // Remove .show.active on all tabs
+    const hiddenATab = document.querySelector('#accepted');
+    hiddenATab.classList.remove("show");
+    hiddenATab.classList.remove("active");
 
-  const hiddenDTab = document.querySelector('#declined');
-  hiddenDTab.classList.remove("show");
-  hiddenDTab.classList.remove("active");
+    const hiddenDTab = document.querySelector('#declined');
+    hiddenDTab.classList.remove("show");
+    hiddenDTab.classList.remove("active");
 
-  // Add .show.active on clicked tab
-  const hiddenPendingTab = document.querySelector('#pending');
-  hiddenPendingTab.classList.add("show");
-  hiddenPendingTab.classList.add("active");
-}
+    // Add .show.active on clicked tab
+    const hiddenPendingTab = document.querySelector('#pending');
+    hiddenPendingTab.classList.add("show");
+    hiddenPendingTab.classList.add("active");
+  }
 
-if (event.target.id == 'accepted-tab') {
-  // Remove .show.active on all tabs
-  const hiddenPenTab = document.querySelector('#pending');
-  hiddenPenTab.classList.remove("show");
-  hiddenPenTab.classList.remove("active");
+  if (event.target.id == 'accepted-tab') {
+    // Remove .show.active on all tabs
+    const hiddenPenTab = document.querySelector('#pending');
+    hiddenPenTab.classList.remove("show");
+    hiddenPenTab.classList.remove("active");
 
-  const hiddenDTab = document.querySelector('#declined');
-  hiddenDTab.classList.remove("show");
-  hiddenDTab.classList.remove("active");
+    const hiddenDTab = document.querySelector('#declined');
+    hiddenDTab.classList.remove("show");
+    hiddenDTab.classList.remove("active");
 
-  // Add .show.active on clicked tab
-  const hiddenPendingTab = document.querySelector('#accepted');
-  hiddenPendingTab.classList.add("show");
-  hiddenPendingTab.classList.add("active");
-}
+    // Add .show.active on clicked tab
+    const hiddenPendingTab = document.querySelector('#accepted');
+    hiddenPendingTab.classList.add("show");
+    hiddenPendingTab.classList.add("active");
+  }
 
-if (event.target.id == 'declined-tab') {
-  // Remove .show.active on all tabs
-  const hiddenPenTab = document.querySelector('#pending');
-  hiddenPenTab.classList.remove("show");
-  hiddenPenTab.classList.remove("active");
+  if (event.target.id == 'declined-tab') {
+    // Remove .show.active on all tabs
+    const hiddenPenTab = document.querySelector('#pending');
+    hiddenPenTab.classList.remove("show");
+    hiddenPenTab.classList.remove("active");
 
-  const hiddenATab = document.querySelector('#accepted');
-  hiddenATab.classList.remove("show");
-  hiddenATab.classList.remove("active");
+    const hiddenATab = document.querySelector('#accepted');
+    hiddenATab.classList.remove("show");
+    hiddenATab.classList.remove("active");
 
-  // Add .show.active on clicked tab
-  const hiddenPendingTab = document.querySelector('#declined');
-  hiddenPendingTab.classList.add("show");
-  hiddenPendingTab.classList.add("active");
-}
-
-
+    // Add .show.active on clicked tab
+    const hiddenPendingTab = document.querySelector('#declined');
+    hiddenPendingTab.classList.add("show");
+    hiddenPendingTab.classList.add("active");
+  }
 }
 
 export {displayTab}
