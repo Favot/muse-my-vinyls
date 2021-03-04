d56a56222fc77bf4804ca69fffa1056555d5ce93
lise henry <lise.henry@essca.eu>
2021-03-04 19:54:21 +0100

    work in progress on listing owner incoming requests

--- app/controllers/owner/rentings_controller.rb
+++ app/controllers/owner/rentings_controller.rb
@@ -1,5 +1,13 @@
 class Owner::RentingsController < ApplicationController
- before_action :set_renting, only: [:decline, :validate]
+  before_action :authenticate_user!
+  before_action :set_renting, only: [:decline, :validate]
+
+  def index
+    @incoming_rentings = Renting.includes(:vinyl).where(vinyls: { owner: current_user })
+    @pending_incoming_rentings = @incoming_rentings.where(status: 'Pending')
+    @accepted_incoming_rentings = @incoming_rentings.where(status: 'Accepted')
+    @declined_incoming_rentings = @incoming_rentings.where(status: 'Declined')
+  end
 
   def validate
     @renting.status = "Accepted"
